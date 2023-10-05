a48bf01fcf99d1adef13dbc5a1e5574b6466e230
Bayu Darmawan <darmawanbayu1@gmail.com>
2023-10-05 20:56:34 +0700

    updated

--- src/Pages/Bayu/Bayu.jsx
+++ src/Pages/Bayu/Bayu.jsx
@@ -1,38 +1,32 @@
-import React, { useRef,useState} from 'react'
+import React, { useRef, useState } from "react";
 
-export const Bayu=()=>{
+export const Bayu = () => {
+  const fileInputRef = useRef(null);
 
-    const fileInputRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+  const handleFileChange = (e) => {
+    const file = e.target.files?.[0];
+    setSelectedFile(file || null);
+    if (file !== undefined) {
+      //   props.onUploadImage(file)
+      console.log(file);
+    }
+  };
+  return (
+    <>
+      <h1>size : {selectedFile?.size / (1024*1024)} mb</h1>
+      <h1> name: {selectedFile?.name}</h1>
+      <h1>type:{selectedFile?.type}</h1>
+      <input
+        ref={fileInputRef}
+        type="file"
+        // accept="image/*"
+        accept=".jpg, .jpeg, .tiff, .png, .gif, .bmp, .heic, .heif"
+        onChange={handleFileChange}
+        className="hidden"
+      />
+    </>
+  );
+};
 
-    const [selectedFile,setSelectedFile] = useState()
-    const handleFileChange = (e) => {
-        const file = e.target.files?.[0];
-        setSelectedFile(file || null);
-        if(file !== undefined){
-        //   props.onUploadImage(file)
-        console.log(file)
-        }
-      };
-    return (
-        <>
-
-        <h1>
-          size : {selectedFile.size}
-        </h1>
-        <h1>    name: {selectedFile.name}
-          
-          </h1>
-          <h1>type:{selectedFile.type}</h1>
-        <input
-          ref={fileInputRef}
-          type="file"
-          // accept="image/*"
-          accept=".jpg, .jpeg, .tiff, .png, .gif, .bmp, .heic, .heif"
-          onChange={handleFileChange}
-          className="hidden"
-        />
-        </>
-    )
-}
-
-export default Bayu
+export default Bayu;
